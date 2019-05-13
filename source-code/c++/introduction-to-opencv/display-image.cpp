3917ea02aa27475a7706e47eed987c5ed8b14571
huangyongsheng <yshhuang@foxmail.com>
2019-05-13 18:56:33 +0800

    main.cpp

---
+++ source-code/c++/introduction-to-opencv/display-image.cpp
@@ -0,0 +1,31 @@
+//
+// Created by yshhuang on 2019-05-13.
+//
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char **argv) {
+    String imageName("/Volumes/develop/code-repository/c++/learn-opencv/source-code/c++/data/lena.png");
+//    if (argc > 1) {
+//        imageName = argv[1];
+//    }
+//
+//    Mat image;
+//    image = imread(imageName, IMREAD_COLOR);
+//    if (image.empty()) {
+//        cout << "Could not open or find the image" << std::endl;
+//    }
+//
+//    namedWindow("Display Window", WINDOW_AUTOSIZE);
+//    imshow("Display Window", image);
+//    waitKey(0);
+    return 0;
+}
---
+++ source-code/c++/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yshhuang on 2019-05-13.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "learn opencv" << endl;
+}
